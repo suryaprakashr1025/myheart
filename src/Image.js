85de2dd9fe9a090dcef987830746ce6562a3bfb3
surya <suryaprakashr1025@gmail.com>
2022-10-11 18:18:22 +0530

    first

--- src/Image.js
+++ src/Image.js
@@ -5,17 +5,12 @@ import third from "./third.jpg"
 import foutrh from "./foutrh.jpg"
 function Image() {
   return (
-    <div class="container">
-      <div class="row">
-        <div class='col-lg-12'>
+   <>
         <img src={first} style={{width:"250px", height:"300px"}} class="img-thumbnail" alt="..."/>
         <img src={second} style={{width:"250px", height:"300px"}} class="img-thumbnail" alt="..."/>
         <img src={third} style={{width:"250px", height:"300px"}} class="img-thumbnail" alt="..."/>
         <img src={foutrh} style={{width:"250px", height:"300px"}} class="img-thumbnail" alt="..."/>
-            </div>
-            </div>
-            </div>
-  )
-}
+    </>
+)}
 
-export default Image
+export default Image;
